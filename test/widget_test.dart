459c208a311802dbea0386632e6f34987230fcdf
Riyan Setiyadi Hidayatullah <setiyariyan19@gmail.com>
2026-07-15 10:49:36 +0700

    chore: rename package to tugas_navigation_pemrograman_berbasis_mobile

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:e_commerce/main.dart';
+import 'package:tugas_navigation_pemrograman_berbasis_mobile/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
